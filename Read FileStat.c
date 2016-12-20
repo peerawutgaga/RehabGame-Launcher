a0d05fc64feaf76ed800cc485d0efc1cce06cb53
Peerawut Luengruengroj <pee.luang45@gmail.com>
2016-12-20 10:16:24 +0700

    Update Read FileStat.c

--- Read FileStat.c
+++ Read FileStat.c
@@ -5,12 +5,12 @@
 #include <sys/types.h>
 #include <string.h>
 
-void listdir(const char *name)
+void listdir(const char *name, FILE *f)
 {
+
     DIR *dir;
     struct dirent *entry;
     struct stat fileStat;
-    FILE *f = fopen("output.txt","w");
     if (!(dir = opendir(name)))
         return;
     printf("%s\n", name);
@@ -19,33 +19,32 @@ void listdir(const char *name)
         char path[1024];
         char dirName[1024];
         int len = snprintf(path, sizeof(path)-1, "%s/%s", name, entry->d_name);
-        if (strcmp(entry->d_name, ".") != 0 || strcmp(entry->d_name, "..") != 0)
-        {
-            strcpy(dirName,"");
-            strcat(dirName,name);
-            strcat(dirName,"\\");
-            strcat(dirName,entry->d_name);
-        }
+        strcpy(dirName,"");
+        strcat(dirName,name);
+        strcat(dirName,"/");
+        strcat(dirName,entry->d_name);
         stat(dirName,&fileStat);
-        printf("%s\n",dirName);
+       // printf("%s\n",dirName);
+        //printf("Size: %d bytes\n",fileStat.st_size);
         if(!S_ISDIR(fileStat.st_mode))
-            printf("Size: %d bytes\n",fileStat.st_size);
-        fprintf(f,"%s,%d\n",dirName,fileStat.st_size);
+        fprintf(f,"%s\t%d\n",dirName,fileStat.st_size);
         path[len] = 0;
         if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
             continue;
-        listdir(path);
+        listdir(path,f);
 
     }
-    fclose(f);
     closedir(dir);
 }
 
 int main()
 {
+    FILE *f = fopen("output.txt","w");
     char argv[] = "c:\\Algorithm Design";
+    //char argv[] = "D:\\leap-motion-game\\Game 1 Full Version";
     struct stat fileStat;
     if(stat(argv,&fileStat)<0)
         return 1;
-    listdir(argv);
+    listdir(argv, f);
+    fclose(f);
 }
