9f6f03a8a25b57fb71ffd18907a28b8d2766fa46
Peerawut Luengruengroj <pee.luang45@gmail.com>
2016-12-20 09:00:09 +0700

    Read all file in folder
    This file is used for analyze all files' size in one folder and its sub-folder.

---
+++ Read FileStat.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+
+void listdir(const char *name)
+{
+    DIR *dir;
+    struct dirent *entry;
+    struct stat fileStat;
+    FILE *f = fopen("output.txt","w");
+    if (!(dir = opendir(name)))
+        return;
+    printf("%s\n", name);
+    while (entry = readdir(dir))
+    {
+        char path[1024];
+        char dirName[1024];
+        int len = snprintf(path, sizeof(path)-1, "%s/%s", name, entry->d_name);
+        if (strcmp(entry->d_name, ".") != 0 || strcmp(entry->d_name, "..") != 0)
+        {
+            strcpy(dirName,"");
+            strcat(dirName,name);
+            strcat(dirName,"\\");
+            strcat(dirName,entry->d_name);
+        }
+        stat(dirName,&fileStat);
+        printf("%s\n",dirName);
+        if(!S_ISDIR(fileStat.st_mode))
+            printf("Size: %d bytes\n",fileStat.st_size);
+        fprintf(f,"%s,%d\n",dirName,fileStat.st_size);
+        path[len] = 0;
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            continue;
+        listdir(path);
+
+    }
+    fclose(f);
+    closedir(dir);
+}
+
+int main()
+{
+    char argv[] = "c:\\Algorithm Design";
+    struct stat fileStat;
+    if(stat(argv,&fileStat)<0)
+        return 1;
+    listdir(argv);
+}
